b9488ffcf99d439c98b05a559b78a79ce3b8ff15
leftenantunicorn <erinbradley27@hotmail.com>
2018-03-14 02:35:34 -0400

    Changed project to full framework and swapped ironpython for pythonnet

---
+++ DiabetesPredictor-Full/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Python.Runtime;
+
+namespace DiabetesPredictor.Controllers
+{
+    [System.Web.Mvc.Route("api/[controller]")]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var path = $"{Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine)};{@"C:\Users\Erin\Anaconda3\"}";
+
+            Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.Process);
+
+            //PythonEngine.PythonHome = @"C:\Users\Erin\Anaconda3\";
+            //PythonEngine.Initialize();
+            string result = "defualt";
+
+            using (Py.GIL())
+            {
+                dynamic np = Py.Import("numpy");
+                Console.WriteLine(np.cos(np.pi * 2));
+
+                dynamic sin = np.sin;
+                Console.WriteLine(sin(5));
+
+                double c = np.cos(5) + sin(5);
+                Console.WriteLine(c);
+                result = c.ToString();
+            }
+            return new string[] { result };
+        }
+
+        // GET api/values/5
+        [HttpGet()]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut()]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete()]
+        public void Delete(int id)
+        {
+        }
+    }
+}

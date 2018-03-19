781d58e9da9dcf3d1a9b4af85018374c8cb2d4fa
erinbradley27 <erinbradley27@hotmail.com>
2018-03-19 16:18:22 -0400

    Updting api - in progress

--- DiabetesPredictor/Controllers/ValuesController.cs
+++ DiabetesPredictor/Controllers/ValuesController.cs
@@ -5,7 +5,10 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
+using System.Text;
 using System.Web.Http;
+using DiabetesPredictor.Models;
 
 namespace DiabetesPredictor.Controllers
 {
@@ -17,7 +20,7 @@ namespace DiabetesPredictor.Controllers
         {
             string result;
 
-            string fileName = "bayespredictortwo.py";
+            string fileName = "bayespredictor.py";
             string dataName = "pima-data.csv";
             string pathPy = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"python\", fileName);
             string pathCsv = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"python\", dataName);
@@ -25,9 +28,9 @@ namespace DiabetesPredictor.Controllers
 
             ProcessStartInfo start = new ProcessStartInfo();
             start.FileName = @"C:\Users\bradleye\Anaconda3\python.exe";
-            var record = string.Format("{0},{1},{2},{3},{4},{5},{6},{7}", modelRecord.num_preg, modelRecord.glucose_conc, modelRecord.diastolic_bp,
-                modelRecord.thickness, modelRecord.insulin, modelRecord.bmi, modelRecord.diab_pred, modelRecord.age);
-            start.Arguments = string.Format("{0} {1} {2}", pathPy, pathCsv, record);
+            //string.Format("{0},{1},{2},{3},{4},{5},{6},{7}", modelRecord.num_preg, modelRecord.glucose_conc, modelRecord.diastolic_bp,
+            //    modelRecord.thickness, modelRecord.insulin, modelRecord.bmi, modelRecord.diab_pred, modelRecord.age);
+            start.Arguments = string.Format("{0} {1} {2}", pathPy, pathCsv, modelRecord.ToString());
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
             using (Process process = Process.Start(start))
@@ -42,17 +45,5 @@ namespace DiabetesPredictor.Controllers
             return result;
         }
 
-    }
-
-    public class DiabetesRecord
-    {
-        public Double num_preg { get; set; }
-        public Double glucose_conc { get; set; }
-        public Double diastolic_bp { get; set; }
-        public Double thickness { get; set; }
-        public Double insulin { get; set; }
-        public Double bmi { get; set; }
-        public Double diab_pred { get; set; }
-        public Double age { get; set; }
     }
 }

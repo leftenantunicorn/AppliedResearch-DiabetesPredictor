cd1db06a0d0cdfd8fb57fc52909310f98a0e6890
erinbradley27 <erinbradley27@hotmail.com>
2018-04-16 13:25:30 -0400

    hyperparams works

--- Predictor/python/hyperparams.py
+++ Predictor/python/hyperparams.py
@@ -7,35 +7,40 @@ try:
     import simplejson as json
     import trained_model as tm
 
-    # Report on accuracies
-	returnedData = []	
-	count = 0.01
-	
-	while True:
-		if count = 1
-			break;
-		predict_test = tm.getNuAccuracyNuSVC(count).predict(tm.x_test)
-		conf_array = metrics.confusion_matrix(tm.y_test, predict_test)
-		
-		true_pos = conf_array[1][1]
-		true_neg = conf_array[0][0]
-		false_pos = conf_array[0][1]
-		false_neg  = conf_array[1][0]
+# Report on accuracies
+returnedData = []	
+count = 1
 
-		data = {"conf" : {
-					"true_pos" : round(np.int64(true_pos).item(),2),
-					"true_neg" : round(np.int64(true_neg).item(),2),
-					"false_pos" : round(np.int64(false_pos).item(),2),
-					"false_neg"  : round(np.int64(false_neg).item(),2),
-					}, 
-				"precision" : round(metrics.precision_score(tm.y_test, predict_test),2)*100,
-				"sensitvity" : round(metrics.recall_score(tm.y_test, predict_test),2)*100,
-				"specificity" : round(true_neg/(true_neg + false_pos),2)*100,
-				"accuracy" : round(metrics.accuracy_score(tm.y_test, predict_test),2)*100
-			   }
-		returnedData.append(data)
-		count = count + 0.01
-		print(json.dumps(predict_test), end="")
+while True:
+    #print("{0:.2f}".format(count/100))
+    if count == 100:
+        break;
+    try:
+        predict_test = getNuSVCModelAccuracy(count/100).predict(x_train)
+    except Exception as e:
+        #print("{0:.2f}".format(count/100) + " is infeasable")
+        count = count + 1
+        continue
+    conf_array = metrics.confusion_matrix(y_train, predict_test)
 
-except Exception as e:
-    print ("Unexpected error:", format(e) )
+    true_pos = conf_array[1][1]
+    true_neg = conf_array[0][0]
+    false_pos = conf_array[0][1]
+    false_neg  = conf_array[1][0]
+
+    data = {"conf" : {
+                "true_pos" : round(np.int64(true_pos).item(),2),
+                "true_neg" : round(np.int64(true_neg).item(),2),
+                "false_pos" : round(np.int64(false_pos).item(),2),
+                "false_neg"  : round(np.int64(false_neg).item(),2),
+                }, 
+            "precision" : round(metrics.precision_score(y_train, predict_test),2)*100,
+            "sensitvity" : round(metrics.recall_score(y_train, predict_test),2)*100,
+            "specificity" : round(true_neg/(true_neg + false_pos),2)*100,
+            "accuracy" : round(metrics.accuracy_score(y_train, predict_test),2)*100,
+            "nu":count/100
+           }
+    returnedData.append(data)
+    count = count + 1
+    
+print(json.dumps(returnedData), end="")
